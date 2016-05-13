cf1b1cc0a718a896c14d6e09c1697e96f0ceba2c
gprivitera <gprivitera@streamatica.com>
2016-05-13 15:59:35 -0500

    Add missing #

--- kafka-init.sh
+++ kafka-init.sh
@@ -9,7 +9,7 @@ then
   echo 'delete.topic.enable = true' >> $KAFKA_HOME/config/server.properties
   sed -i 's/^zookeeper.connect=.*/zookeeper.connect='$ZK_CONNECT'/' $KAFKA_HOME/config/server.properties
   sed -i 's/^broker.id=.*/broker.id='$BROKER_ID'/' $KAFKA_HOME/config/server.properties
-  sed -i 's/^advertised.host.name=.*/advertised.host.name='$ADVERTISED_HOSTNAME'/' $KAFKA_HOME/config/server.properties
+  sed -i 's/^#advertised.host.name=.*/advertised.host.name='$ADVERTISED_HOSTNAME'/' $KAFKA_HOME/config/server.properties
   trap "$KAFKA_HOME/bin/kafka-server-stop.sh; echo 'Kafka stopped.'; exit" SIGHUP SIGINT SIGTERM
   $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties > $KAFKA_HOME/kafka.log
 fi
