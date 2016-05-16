cb0374d8d8459102b81dfffdc290fb51521c5eb2
gprivitera <gprivitera@streamatica.com>
2016-05-16 10:50:01 -0500

    Remove advertised hostname option

--- kafka-init.sh
+++ kafka-init.sh
@@ -2,14 +2,14 @@
 
 ZK_CONNECT=$1
 BROKER_ID=$2
-ADVERTISED_HOSTNAME=$3
+# ADVERTISED_HOSTNAME=$3
 
 if [ -n "$ZK_CONNECT" ]
 then
   echo 'delete.topic.enable = true' >> $KAFKA_HOME/config/server.properties
   sed -i 's/^zookeeper.connect=.*/zookeeper.connect='$ZK_CONNECT'/' $KAFKA_HOME/config/server.properties
   sed -i 's/^broker.id=.*/broker.id='$BROKER_ID'/' $KAFKA_HOME/config/server.properties
-  sed -i 's/^#advertised.host.name=.*/advertised.host.name='$ADVERTISED_HOSTNAME'/' $KAFKA_HOME/config/server.properties
+#  sed -i 's/^#advertised.host.name=.*/advertised.host.name='$ADVERTISED_HOSTNAME'/' $KAFKA_HOME/config/server.properties
   trap "$KAFKA_HOME/bin/kafka-server-stop.sh; echo 'Kafka stopped.'; exit" SIGHUP SIGINT SIGTERM
   $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties > $KAFKA_HOME/kafka.log
 fi
